f7c4b2f10aee3a9fd05550de83d77b4ad2b1b6eb
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-01-07 11:12:17 -0300

    Fixed a bug in professional schedules

--- src/common/enum/days.enum.ts
+++ src/common/enum/days.enum.ts
@@ -1,9 +1,9 @@
 export enum Days {
-  MONDAY = 'Monday',
-  TUESDAY = 'Tuesday',
-  WEDNESDAY = 'Wednesday',
-  THURSDAY = 'Thursday',
-  FRIDAY = 'Friday',
-  SATURDAY = 'Saturday',
-  SUNDAY = 'Sunday',
+  MONDAY = 'Mon',
+  TUESDAY = 'Tue',
+  WEDNESDAY = 'Wed',
+  THURSDAY = 'Thu',
+  FRIDAY = 'Fri',
+  SATURDAY = 'Sat',
+  SUNDAY = 'Sun',
 }
