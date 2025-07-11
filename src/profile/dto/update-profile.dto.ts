42c025f4c15acc928268afd849cdca59a9ecda76
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-07-11 14:35:56 -0300

    Modified update profile dto

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,44 +1,36 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateProfileDto {
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   profileName: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   bio: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   location: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   phone: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   instagram: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   facebook: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   x: string;
 
   @IsString()
   @IsOptional()
-  @IsNotEmpty()
   tiktok: string;
 
 }
